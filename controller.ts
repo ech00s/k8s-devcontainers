21fe8152e10d0bb4d7b69cd5effc259f92356d33
pikaonacid <yamato5002@gmail.com>
2025-11-29 22:59:37 +0100

    fix entrypoint script redirection bug

--- controller.ts
+++ controller.ts
@@ -72,18 +72,18 @@ export class controller extends plugin<controller_config>{
                 },
                 "data":{
                     "entrypoint.sh":''+
-                        'apt-get update -y && apt-get install -y openssh-server;'+
+                        'set -e;apt-get update -y && apt-get install -y openssh-server;'+
                         'mkdir /home/vscode/.ssh'+
                         ' && touch /home/vscode/.ssh/authorized_keys'+
                         ' && chown -R vscode:vscode /home/vscode/.ssh'+
                         ' && chmod 700 /home/vscode/.ssh'+
                         ' && chmod 600 /home/vscode/.ssh/authorized_keys;'+
-                        'sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/g" /etc/ssh/sshd_config'+
-                        ' && sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin no/g" /etc/ssh/sshd_config'+
-                        ' && sed -i "s/#PubkeyAuthentication yes/PubkeyAuthentication yes/g" /etc/ssh/sshd_config'+
-                        ' && sed -i "s/#AuthorizedKeysFile .*/AuthorizedKeysFile .ssh/authorized_keys/g";'+
+                        'sed -i -e "s/#PasswordAuthentication yes/PasswordAuthentication no/g" /etc/ssh/sshd_config'+
+                        ' && sed -i -e "s/#PermitRootLogin prohibit-password/PermitRootLogin no/g" /etc/ssh/sshd_config'+
+                        ' && sed -i -e "s/#PubkeyAuthentication yes/PubkeyAuthentication yes/g" /etc/ssh/sshd_config'+
+                        ' && sed -i -e "s/#AuthorizedKeysFile .*/AuthorizedKeysFile .ssh/authorized_keys/g";'+
                         'sh -c \'IFS="," read -ra SPLIT_KEYS<<< "$AUTHORIZED_KEYS"\';'+
-                        'for key in "${AUTHORIZED_KEYS[@]}"; do;sh -c \'echo $key >> ~/.ssh/authorized_keys\';done;'+
+                        'for key in "${AUTHORIZED_KEYS[@]}"; do K=$key sh -c \'echo $K >> ~/.ssh/authorized_keys\';done;'+
                         'service ssh start -D;'
                 }
             },
