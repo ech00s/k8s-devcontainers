cfbfa5180fe390a1c6e536404a6b5a726ffea2d5
Pikachuonacid <kingston@ech00s.com>
2026-07-20 19:31:01 +0200

    add kubeconfig setup to dev image builds and entrypoint

--- images/entrypoint.sh
+++ images/entrypoint.sh
@@ -13,4 +13,25 @@ printf "%s" "$AUTHORIZED_KEYS" \
 chown -R "$USER:$USER" "$HOME/.ssh"
 chmod 700 "$HOME/.ssh"
 chmod 600 "$HOME/.ssh/authorized_keys"
+
+cat >/etc/kubeconfig <<EOF
+apiVersion: v1
+kind: Config
+clusters:
+- cluster:
+    certificate-authority: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+    server: https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT}
+  name: cluster
+contexts:
+- context:
+    cluster: cluster
+    user: sa
+  name: default
+current-context: default
+users:
+- name: sa
+  user:
+    tokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
+EOF
+
 exec /usr/sbin/sshd -D
